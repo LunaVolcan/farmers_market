364673fc13681f0a1bbc86813b7bf8684855e1a4
Luna Fernandez <Luna.Fernandez@anniecannonss-MacBook-Pro.local>
2024-06-18 20:33:28 -0600

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector(".header-burger i");
+    const nav = document.querySelector(".bar-nav");
+  
+    function showHamburger() {
+      burger.classList.toggle("fa-times");
+      nav.classList.toggle("nav-active");
+      console.log("showHamburger");
+    }
+  
+    document.getElementById('hamburgerButton').addEventListener('click', showHamburger);
+  });
