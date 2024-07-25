0ab97be02ca87b5c3bb19367b7401f381905fc9e
Luna Fernandez <Luna.Fernandez@anniecannonss-MacBook-Pro.local>
2024-07-25 13:39:36 -0600

    burger menu and contact form

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,49 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const burger = document.querySelector(".header-burger i");
+//     const nav = document.querySelector(".bar-nav");
+  
+//     function showHamburger() {
+//       burger.classList.toggle("fa-times");
+//       nav.classList.toggle("nav-active");
+//       console.log("showHamburger");
+//     }
+  
+//     document.getElementById('hamburgerButton').addEventListener('click', showHamburger);
+//   });
+
 document.addEventListener('DOMContentLoaded', () => {
+  const burgerButton = document.getElementById('hamburgerButton');
+  
+  if (burgerButton) {
     const burger = document.querySelector(".header-burger i");
     const nav = document.querySelector(".bar-nav");
-  
+
     function showHamburger() {
       burger.classList.toggle("fa-times");
       nav.classList.toggle("nav-active");
       console.log("showHamburger");
     }
-  
-    document.getElementById('hamburgerButton').addEventListener('click', showHamburger);
-  });
+
+    burgerButton.addEventListener('click', showHamburger);
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  let userArray = [];
+
+  if (form) {
+    form.addEventListener('submit', onFormSubmit);
+
+    function onFormSubmit(event) {
+      event.preventDefault();
+      const data = new FormData(event.target);
+      const dataObject = Object.fromEntries(data.entries());
+
+      userArray.push(dataObject);
+      console.log(userArray);
+      form.reset();
+    }
+  }
+});
